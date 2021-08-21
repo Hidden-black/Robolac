b8cbab289b0c6e8f1656c4750b1540d37fc819ca
TheHiddenBlack1 <shashwatrai_28@outlook.com>
2021-08-21 17:43:00 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -16,6 +16,14 @@ from discord.ext import commands
 from asyncio import TimeoutError
 from discord.ext.commands.core import command
 
+def get_prefix(bot,message):
+    with open('prefix.json','r') as f:
+        prefix = json.load(f)
+    return prefix[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix='.')
+bot.launch_time = datetime.utcnow()
+
 class Help(commands.Cog, name= "Help"):
 
     def __init__(self,bot):
@@ -23,7 +31,7 @@ class Help(commands.Cog, name= "Help"):
 
     @commands.group(invoke_without_command=True)
     async def help(self,ctx):
-        em = discord.Embed(title = "Help" , description = f" **Prefix For all Commnds Is `.`** \n Use `.help <command>` for more info of the command \n Use `.help <category>` for more help in the category" , color = ctx.author.color)
+        em = discord.Embed(title = "Help" , description = f" **Prefix For all Commnds Is `{ctx.prefix}`** \n Use `{ctx.prefix}help <command>` for more info of the command \n Use `{ctx.prefix}help <category>` for more help in the category" , color = ctx.author.color)
 
         em.add_field(name = "Botsay", value = f"`say`,`eh`,`bruh`,`ok`,`doge`" , inline= False)
         em.add_field(name = "Reddit", value = "`meme`,`joke`" , inline= False)
@@ -31,7 +39,7 @@ class Help(commands.Cog, name= "Help"):
         em.add_field(name = "Image" , value = "`img`,`cat`,`dog`" , inline= False)
         em.add_field(name = "Fact" , value = "`fact`,`fact cat`,`fact dog`" , inline= False)
         em.add_field(name = "Invite The bot to your server with `invite`" , value = "_ _" , inline= False)
-        em.add_field(name = "You can change the prefix with `setprefix`",value =f"_ _", inline= False)
+        em.add_field(name = f"You can change the prefix with `{ctx.prefix}setprefix`",value =f"_ _", inline= False)
         em.add_field(name = "_ _",value =f"**[Check out My Website!](https://main-index.thehiddenblack1.repl.co)**", inline= False)
 
         await ctx.send(embed = em)
@@ -42,11 +50,11 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "**SAY**" , description = "Makes the bot say something for you" , color = ctx.author.color)
 
-        em.add_field(name = "Say" , value = "`.say <what u want the bot to say>`", inline= False)
-        em.add_field(name = "Eh" , value = "`.eh` make the bot say eh for you", inline= False)
-        em.add_field(name = "Bruh" , value = "`.bruh` makes the bot say bruh yhea bruh", inline= False)
-        em.add_field(name = "Ok" , value = "`.ok` Ok and dude ", inline= False)
-        em.add_field(name = "Doge" , value = "`.doge` tells the world doge is an idiot", inline= False)
+        em.add_field(name = "Say" , value = f"`{ctx.prefix}say <what u want the bot to say>`", inline= False)
+        em.add_field(name = "Eh" , value = f"`{ctx.prefix}eh` make the bot say eh for you", inline= False)
+        em.add_field(name = "Bruh" , value = f"`{ctx.prefix}bruh` makes the bot say bruh yhea bruh", inline= False)
+        em.add_field(name = "Ok" , value = f"`{ctx.prefix}ok` Ok and dude ", inline= False)
+        em.add_field(name = "Doge" , value = f"`{ctx.prefix}doge` tells the world doge is an idiot", inline= False)
 
         await ctx.send(embed = em)
 
@@ -55,8 +63,8 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "**Reddit**" , description = "Makes the bot say something for you" , color = ctx.author.color)
 
-        em.add_field(name = "Meme" , value = "`.meme` gives random meme from reddit", inline= False)
-        em.add_field(name = "Joke" , value = "`.joke` gives random joke drom reddit", inline= False)
+        em.add_field(name = "Meme" , value = f"`{ctx.prefix}meme` gives random meme from reddit", inline= False)
+        em.add_field(name = "Joke" , value = f"`{ctx.prefix}joke` gives random joke drom reddit", inline= False)
         await ctx.send(embed = em)
 
     @help.command()
@@ -64,20 +72,20 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "**FUN**" , description = "Fun commands :)" , color = ctx.author.color)
 
-        em.add_field(name = "Egirls" , value = "`.egirls` Gives images of hot egirls", inline= False)
-        em.add_field(name = "Rock Paper and Scissors" , value = "`.rps`", inline= False)
-        em.add_field(name = "Avatar" , value = "`.av <user>` Gives the avatar of the mentioned user", inline= False)
-        em.add_field(name = "Eightball" , value = "`.8ball <question>`", inline= False)
-        em.add_field(name = "Kills" , value = "`.kill <user>` Kills the mentioned user in Minecraft Words", inline= False)
+        em.add_field(name = "Egirls" , value = f"`{ctx.prefix}egirls` Gives images of hot egirls", inline= False)
+        em.add_field(name = "Rock Paper and Scissors" , value = f"`{ctx.prefix}rps`", inline= False)
+        em.add_field(name = "Avatar" , value = f"`{ctx.prefix}av <user>` Gives the avatar of the mentioned user", inline= False)
+        em.add_field(name = "Eightball" , value = f"`{ctx.prefix}8ball <question>`", inline= False)
+        em.add_field(name = "Kills" , value = f"`{ctx.prefix}kill <user>` Kills the mentioned user in Minecraft Words", inline= False)
         await ctx.send(embed = em)
 
     @help.command()
     async def image(self , ctx):
         em = discord.Embed(title = "**Image**" , description = "Random Picure of animals and nature" , color = ctx.author.color)
 
-        em.add_field(name = "Dog" , value = "`.dog` Cute duggo Pics :)", inline= False)
-        em.add_field(name = "Cat" , value = "`.cat` Cute cats :)", inline= False)
-        em.add_field(name = "Image Search" , value = "`.img <Image you want>`  Make sure there is no space", inline= False)
+        em.add_field(name = "Dog" , value = f"`{ctx.prefix}dog` Cute duggo Pics :)", inline= False)
+        em.add_field(name = "Cat" , value = f"`{ctx.prefix}cat` Cute cats :)", inline= False)
+        em.add_field(name = "Image Search" , value = f"`{ctx.prefix}img <Image you want>`  Make sure there is no space", inline= False)
 
         await ctx.send(embed = em)
 
@@ -85,9 +93,9 @@ class Help(commands.Cog, name= "Help"):
     async def fact(self , ctx):
         em = discord.Embed(title = "**Facts**" , description = "Random Picure of animals and nature" , color = ctx.author.color)
 
-        em.add_field(name = "Fact" , value = "`.fact` Cool facts", inline= False)
-        em.add_field(name = "Cat Facts" , value = "`.fact cat` Facts about cats", inline= False)
-        em.add_field(name = "Dog Facts" , value = "`.fact dog` Dog Facts ", inline= False)
+        em.add_field(name = "Fact" , value = f"`{ctx.prefix}fact` Cool facts", inline= False)
+        em.add_field(name = "Cat Facts" , value = f"`{ctx.prefix}fact cat` Facts about cats", inline= False)
+        em.add_field(name = "Dog Facts" , value = f"`{ctx.prefix}fact dog` Dog Facts ", inline= False)
         em.add_field(name = "**More info on `.help facts`**" , value = "_ _", inline= False)
         await ctx.send(embed = em)
 
@@ -96,7 +104,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "say" , description = "cooldown of 2 minutes" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".say")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}say")
 
         await ctx.send(embed = em)
 
@@ -105,7 +113,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Eh?" , description = "eh?" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".eh")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}eh")
 
         await ctx.send(embed = em)
         
@@ -114,7 +122,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Bruh" , description = "Bot says bruh XD", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".bruh")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}bruh")
 
         await ctx.send(embed = em)
 
@@ -123,7 +131,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Ok and?" , description = "Ok and annoying gif", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".ok")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}ok")
 
         await ctx.send(embed = em)
 
@@ -132,7 +140,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Doge" , description = "||Doge||", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".doge")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}doge")
 
         await ctx.send(embed = em)
 
@@ -141,7 +149,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Rock Paper Scissors" , description = "Choose the buttons", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".rps")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}rps")
 
         await ctx.send(embed = em)
 
@@ -150,7 +158,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Meme" , description = "Sends random meme", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".meme")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}meme")
 
         await ctx.send(embed = em)
 
@@ -159,7 +167,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Egils" , description = "Hot egirls 🥰🤤😋", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".egirls")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}egirls")
 
         await ctx.send(embed = em)
 
@@ -168,7 +176,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Invite" , description = "Create bat invite", color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = ".invite")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}invite")
 
         await ctx.send(embed = em)
 
@@ -177,7 +185,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Av" , description = "Gives avatar of metioned user" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = f".av {ctx.author.mention}")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}av {ctx.author.mention}")
 
         await ctx.send(embed = em)
 
@@ -186,7 +194,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Eightball" , description = "Gives an answer to the question" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = f".eightball Am i an idiot?")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}eightball Am i an idiot?")
 
         await ctx.send(embed = em)
 
@@ -195,7 +203,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Kill" , description = "Kills The mentioned user" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value = f".kill {ctx.author.mention}")
+        em.add_field(name = "**Example**" , value = f"{ctx.prefix}kill {ctx.author.mention}")
 
         await ctx.send(embed = em)
 
@@ -204,7 +212,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Joke" , description = "Joke directly from r/jokes" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value =".joke")
+        em.add_field(name = "**Example**" , value =f"{ctx.prefix}joke")
 
         await ctx.send(embed = em)
 
@@ -213,7 +221,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Dog" , description = "Cute duggos" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value =".dog")
+        em.add_field(name = "**Example**" , value =f"{ctx.prefix}dog")
 
         await ctx.send(embed = em)
 
@@ -222,7 +230,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Cat" , description = "Kittys :)" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value =".cat")
+        em.add_field(name = "**Example**" , value =f"{ctx.prefix}cat")
 
         await ctx.send(embed = em)
 
@@ -232,7 +240,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Image Search" , description = "Saech Images from web" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value =".img <image name>")
+        em.add_field(name = "**Example**" , value =f"{ctx.prefix}img <image name>")
 
         await ctx.send(embed = em)
 
@@ -241,7 +249,7 @@ class Help(commands.Cog, name= "Help"):
 
         em = discord.Embed(title = "Facts" , description = "Cool facts :o" , color = ctx.author.color)
 
-        em.add_field(name = "**Example**" , value =".fact \n.fact cat \n.fact dog")
+        em.add_field(name = "**Example**" , value =f"{ctx.prefix}fact \n{ctx.prefix}fact cat \n{ctx.prefix}fact dog")
 
         await ctx.send(embed = em)
 
