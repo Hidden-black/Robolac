568a35f73273c8cc67620a8c218bd06b0ba2090a
TheHiddenBlack1 <shashwatrai_28@outlook.com>
2021-09-12 20:32:15 +0530

    Update img.py

--- Robolac src/cogs/img.py
+++ Robolac src/cogs/img.py
@@ -24,40 +24,46 @@ class IMG(commands.Cog, name="IMAGE"):
         self.bot = bot
 
     @commands.command()
-    async def dog(self, ctx):
-        r = requests.get("https://dog.ceo/api/breeds/image/random")
-        res = r.json()
-        title = res["message"]
-        m = discord.Embed(
-            title='', description=f"**[Woof...]({title})**", color=ctx.author.color)
-        m.set_image(url=f"{title}")
-
-        await ctx.send(embed=m)
-
-    @commands.command()
-    async def cat(self, ctx):
-        em = discord.Embed(title='Meow...', color=ctx.author.color)
-        em.set_image(url=f"https://source.unsplash.com/1920x1080/?cat")
-        await ctx.send(embed=em)
-
-    @commands.command()
-    async def img(self, ctx, arg):
+    async def dog(self ,ctx):
         async with aiohttp.ClientSession() as cs:
-            # get client_id from UNSPLASH API and make a application their
-            async with cs.get(f'https://api.unsplash.com/search/photos?query={arg}&client_id=') as r:
+            async with cs.get("https://dog.ceo/api/breeds/image/random") as r:
                 res = await r.json()
-                ri = random.randint(0, 50)
+                
+                title = res["message"]
+                m = discord.Embed(title = '' ,description=f"**[Woof...]({title})**" , color = ctx.author.color)
+                m.set_image(url = f"{title}")
+                
+                
+                await ctx.send(embed = m)
+
+    @commands.command()
+    async def cat(self , ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://api.thecatapi.com/v1/images/search") as r:
+                res = await r.json()
+                e = res[0]['url']
+
+                f = discord.Embed(title ='Meow...',description ='_ _',colour = discord.Colour.from_hsv(random.random(), 1, 1))
+                f.set_image(url = e)
+                await ctx.send(embed = f)
+
+
+    @commands.command()
+    async def img(self,ctx, arg):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.unsplash.com/search/photos?query={arg}&client_id=kt4I9LIgyzPpqPxmqkMYnTzqlTPmUNFQLJxjIahksd0') as r:
+                res = await r.json()
+                ri = random.randint(0,50)
                 raw_results = res["results"]
                 first_option = raw_results[0]
                 raw_url = first_option["urls"]
                 url = raw_url["regular"]
-                m = discord.Embed(
-                    title=f'Here is your image for {arg}', description="", color=ctx.author.color)
-                m.set_image(url=url)
-                m.set_footer(
-                    test="😥 Same image on same query till i fix it 🙏Pls wait")
+                m = discord.Embed(title = f'Here is your image for {arg}', description="" ,color = ctx.author.color)
+                m.set_image(url = url)
+                m.set_footer(test="😥 Same image on same query till i fix it 🙏Pls wait")
 
-                await ctx.send(embed=m)
+                await ctx.send(embed = m)
+
 
 
 def setup(bot):
