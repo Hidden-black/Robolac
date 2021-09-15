04c9a947842d6c3e12c92b18f2f07f3a04ff6241
TheHiddenBlack1 <shashwatrai_28@outlook.com>
2021-09-15 21:16:38 +0530

    cooler

--- Robolac src/main.py
+++ Robolac src/main.py
@@ -4,11 +4,15 @@ import json
 import random
 import asyncio
 import aiohttp
+import typing
 import time
 import discord
 import jishaku
 import requests
 import pymongo
+import clever_chat
+from clever_chat import AsyncClient
+from typing import List
 import randomstuff
 from discord.ext import menus
 from discord import message
@@ -16,8 +20,6 @@ from random import choice
 from datetime import datetime
 from discord.ext import commands
 from asyncio import TimeoutError
-from discord.ext.commands.core import command
-
 
 
 def get_prefix(bot, message):
@@ -30,9 +32,12 @@ def get_prefix(bot, message):
         return "."
 
 
-bot = commands.Bot(command_prefix=get_prefix,
-                   intents=discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix=",", intents=discord.Intents.all(
+), case_insensitive=True)
+bot.owner_ids = [842950909159145493, 740906193312284715]
 bot.launch_time = datetime.utcnow()
+
+
 
 class MyHelpCommand(commands.MinimalHelpCommand):
     async def send_pages(self):
@@ -41,9 +46,8 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         for page in self.paginator.pages:
             e.description += page
         await destination.send(embed=e)
-        
-bot.help_command = MyHelpCommand()
 
+bot.help_command = MyHelpCommand()
 
 @bot.event  # PREFIX BEING ADDED ON GUILD JOIN
 async def on_guild_join(guild):
@@ -54,6 +58,14 @@ async def on_guild_join(guild):
     with open('prefix.json', 'w') as f:
         json.dump(prefix, f, indent=4)
 
+    channel = bot.get_channel(877428062336192542)
+
+    e = discord.Embed(
+        title=f"Bot added in :`{guild.name}`", description=f"_ _", color=0x00FF00)
+    e.add_field(
+        name=f"No of members : {len(guild.members)}", value=f"<t:{int(time.time())}:F>")
+    await channel.send(embed=e)
+
 
 @bot.event  # PREFIX BEING REMOVED ON GUILD JOIN
 async def on_guild_remove(guild):
@@ -63,6 +75,14 @@ async def on_guild_remove(guild):
 
     with open('prefix.json', 'w') as f:
         json.dump(prefix, f, indent=4)
+
+    channel = bot.get_channel(877428062336192542)
+
+    e = discord.Embed(
+        title=f"Bot removed in :`{guild.name}`", description=f"_ _", color=0xFF0000)
+    e.add_field(
+        name=f"No of members : {len(guild.members)}", value=f"<t:{int(time.time())}:F>")
+    await channel.send(embed=e)
 
 
 @bot.command()  # COMMAND TO SET PREFIX
@@ -80,15 +100,26 @@ async def setprefix(ctx, prefixset):
 
 os.environ["JISHAKU_FORCE_PAGINATOR"] = "True"
 
+print("""
+        ______ ___________ _____ _      ___  _____ 
+        | ___ \  _  | ___ \  _  | |    / _ \/  __ \\
+        | |_/ / | | | |_/ / | | | |   / /_\ \ /  \/
+        |    /| | | | ___ \ | | | |   |  _  | |    
+        | |\ \\\\ \_/ / |_/ \ \_/ / |____ | | | \__/\\
+        \_| \_|\___/\____/ \___/\_____\_| |_/\____/
+                        An Idiot Bot By HiddenBlack
+    """)
+
+
 
 extensions = [
     'cogs.say',
     'cogs.owner',
     'cogs.fun',
     'cogs.reddit',
-    'cogs.do',
     'cogs.slash',
-    'cogs.help',
+    # 'cogs.help',
+    'cogs.chatbot',
     'cogs.error',
     'cogs.fact',
     'cogs.img',
@@ -103,7 +134,7 @@ if __name__ == "__main__":
             bot.load_extension(extension)
         except Exception as e:
             print(f'error loading {extension}', file=sys.stderr)
-
+            print(e)
 
 if os.path.exists(os.getcwd()+"/config.json"):
 
@@ -130,6 +161,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Follow my Twitch lol", url="https://www.twitch.tv/hidden_black_"))
     print("-----------------------------------")
     print("Bot is ready!")
+
 
 
 bot.run(Token)
