708c55b7549454f764da5ce7cc9d205e48f8f36c
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2021-09-18 11:16:37 +0530

    Update reddit.py

--- Robolac src/cogs/reddit.py
+++ Robolac src/cogs/reddit.py
@@ -16,39 +16,37 @@ from discord.ext import commands
 from asyncio import TimeoutError
 from discord.ext.commands.core import command
 
+class Reddit(commands.Cog, name= "Reddit"):
 
-class Reddit(commands.Cog, name="Reddit"):  # Random Color BABY!!!!!!!!!
-
-    def __init__(self, bot):
+    def __init__(self,bot):
         self.bot = bot
 
     @commands.command()
-    async def meme(self, ctx):
+    async def meme(self,ctx):
         memepage = [
             "https://www.reddit.com/r/memes/hot.json",
             "https://www.reddit.com/r/dankmemes/hot.json",
             "https://www.reddit.com/r/meme/hot.json"
-        ]
+            ]
+
 
         async with aiohttp.ClientSession() as cs:
             async with cs.get(f"{random.choice(memepage)}") as r:
                 res = await r.json()
-                rm = random.randint(1, 25)
-                ml = res['data']['children'][rm]['data']['permalink']
-                title = res['data']['children'][rm]['data']['title']
-                ups = res['data']['children'][rm]['data']['ups']
-                com = res['data']['children'][rm]['data']['num_comments']
-                nsfw = res['data']['children'][rm]['data']["over_18"]
-                subr = res['data']['children'][rm]['data']["subreddit_name_prefixed"]
+                rm = random.randint(1,25)
+                ml = res['data']['children'] [rm]['data']['permalink']
+                title = res['data']['children'] [rm]['data']['title']
+                ups = res['data']['children'] [rm]['data']['ups']
+                com = res['data']['children'] [rm]['data']['num_comments']
+                nsfw = res['data']['children'] [rm]['data']["over_18"]
+                subr = res['data']['children'] [rm]['data']["subreddit_name_prefixed"]
 
                 if nsfw == False:
-                    em = discord.Embed(
-                        title=f'{title}', description=f"**[Reddit link](http://www.reddit.com{ml})**", colour=discord.Colour.from_hsv(random.random(), 1, 1))
-                    em.set_image(url=res['data']['children']
-                                 [rm]['data']['url'])
-                    em.set_footer(text=f'⬆️{ups} | 💬{com} | {subr}')
+                    em = discord.Embed(title = f'{title}' , description = f"**[Reddit link](http://www.reddit.com{ml})**" , colour = discord.Colour.from_hsv(random.random(), 1, 1))
+                    em.set_image(url = res['data']['children'] [rm] ['data']['url'])
+                    em.set_footer(text = f'⬆️{ups} | 💬{com} | {subr}')
 
-                    await ctx.send(embed=em)
+                    await ctx.send(embed = em)
                 else:
                     pass
 
@@ -69,11 +67,10 @@ class Reddit(commands.Cog, name="Reddit"):  # Random Color BABY!!!!!!!!!
                 nsfw = res['data']['children'] [random_joke]['data']["over_18"]
 
 
-                em = discord.Embed(title = '' ,description=f"**[{title}](http://www.reddit.com{jl})**" , colour = discord.Colour.from_hsv(random.random(), 1, 1))
-                em.add_field(name= f"{text}" , value = '_ _', inline = False)
+                em = discord.Embed(title = '_ _' ,description=f"**[{title}](http://www.reddit.com{jl})**" , colour = discord.Colour.from_hsv(random.random(), 1, 1))
+                em.add_field(name= f"_ _" , value = f'{text}', inline = False)
                 em.set_footer(text = f'⬆️{ups} | 💬{com}')
                 await ctx.send(embed=em)
-
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
