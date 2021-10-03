217e74e34604a8d2747dac3d8f7af1dcb4a37348
TheHiddenBlack1 <shashwatrai_28@outlook.com>
2021-10-03 17:21:26 +0530

    Added Eval Disable/Enable  Commands

--- Robolac src/cogs/owner.py
+++ Robolac src/cogs/owner.py
@@ -6,6 +6,8 @@ import asyncio
 import aiohttp
 import discord
 import jishaku
+import traceback
+import io
 import requests
 import randomstuff
 from discord import *
@@ -15,7 +17,9 @@ from datetime import datetime
 from discord.ext import commands
 from asyncio import TimeoutError
 from discord.ext.commands.core import command
-
+import textwrap
+import importlib
+from contextlib import redirect_stdout
 
 class Owner(commands.Cog, name="Owner"):
 
@@ -83,9 +87,72 @@ class Owner(commands.Cog, name="Owner"):
         channel = self.bot.get_channel(int(huh))
 
         await channel.send(arg)
-"""
-Adding more stuff
-"""
+
+    @commands.command()
+    @commands.is_owner()
+    async def disable(self, ctx, command):
+        command = self.bot.get_command(command)
+        if not command.enabled:
+            return await ctx.send("This command is already disabled.")
+        command.enabled = False
+        await ctx.send(f"Disabled {command.name} command.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def enable(self, ctx, command):
+        command = self.bot.get_command(command)
+        if command.enabled:
+            return await ctx.send("This command is already enabled.")
+        command.enabled = True
+        await ctx.send(f"Enabled {command.name} command.")
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @commands.is_owner()
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 def setup(bot):
     bot.add_cog(Owner(bot))
